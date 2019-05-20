0f8e88ec8efdeca90f8e4f6f6dfe2767f6632977
Bongani NKOSI <bnkosi@c5r3s3.wethinkcode.co.za>
2019-05-20 10:30:34 +0200

    added strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnkosi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 10:06:18 by bnkosi            #+#    #+#             */
+/*   Updated: 2019/05/20 10:28:45 by bnkosi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] && i < dstsize)
+		i++;
+	while ((src[j]) && ((i + j + 1) < size))
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i != size)
+		dst(i + j) = '\0';
+	return (i + ft_strlen(src));
+}
