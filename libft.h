2a9733f45734203099410bc5b1f3ac058036bdc4
Bongani NKOSI <bnkosi@c5r3s3.wethinkcode.co.za>
2019-05-21 08:48:57 +0200

    added strrchr, library updated with strrchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bnkosi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 14:11:06 by bnkosi            #+#    #+#             */
-/*   Updated: 2019/05/21 08:37:48 by bnkosi           ###   ########.fr       */
+/*   Updated: 2019/05/21 08:48:18 by bnkosi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,3 +46,4 @@ char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 size_t	*ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
