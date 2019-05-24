1afa36fd62fbebb3acd7f7954d6d2c38dfbc4a54
Bongani NKOSI <bnkosi@c5r3s3.wethinkcode.co.za>
2019-05-24 12:06:48 +0200

    updated lib

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bnkosi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 14:11:06 by bnkosi            #+#    #+#             */
-/*   Updated: 2019/05/24 09:53:05 by bnkosi           ###   ########.fr       */
+/*   Updated: 2019/05/24 12:06:17 by bnkosi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int		ft_atoi(const char *str);
 void	ft_putchar(char c);
 void	ft_putchar_fd(char *c, int fd);
 void	ft_putstr(char const *str);
+int		ft_strlen(char *str);
 char	ft_itoa(int i);
 char	ft_strcpy(char *s1, char *s2);
 int		ft_strlen(char *str);
@@ -68,3 +69,5 @@ int		ft_strequ(char const *s1, char const *s2);
 int		ft_strenqu(char const *s1, char const *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
+
