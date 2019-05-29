9a69164bd82e679edd08c832fc67695d175d6f01
Bongani NKOSI <bnkosi@c5r3s3.wethinkcode.co.za>
2019-05-29 17:35:55 +0200

    fixing strchr...

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bnkosi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:47:39 by bnkosi            #+#    #+#             */
-/*   Updated: 2019/05/21 08:35:32 by bnkosi           ###   ########.fr       */
+/*   Updated: 2019/05/29 17:35:27 by bnkosi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *s, int c)
 	size_t i;
 
 	i = 0;
+	if (s == NULL)
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
